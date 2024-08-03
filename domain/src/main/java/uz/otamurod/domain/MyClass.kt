7f1494a61f723f5cf085d72164bb1eecf53142a7
otamurod <safarovotamurod@gmail.com>
2024-08-03 12:18:59 +0500

    feat: create domain module

---
+++ domain/src/main/java/uz/otamurod/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package uz.otamurod.domain
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "MyTaxi"
 include(":app")
 include(":data")
+include(":domain")
