ca4d51045c126d1f6449646b9b0b288ed4035675
otamurod <safarovotamurod@gmail.com>
2024-08-03 12:23:39 +0500

    feat: inject layers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,10 @@ android {
 
 dependencies {
 
+    implementation(project(":data"))
+    implementation(project(":domain"))
+    implementation(project(":presentation"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
